1e27ae2a9d8dbd7bfcc3b7569e2a7f66b9b9f621
Harrison Mutai <harrisonmutai@Harrisons-Air.broadband>
2023-10-27 16:41:24 +0100

    lab: add solutions to lab 04

--- lab/04/lab04.py
+++ lab/04/lab04.py
@@ -19,7 +19,9 @@ def summation(n, term):
     True
     """
     assert n >= 1
-    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(n)
+    return term(n) + summation(n-1, term)
 
 
 def paths(m, n):
@@ -35,7 +37,9 @@ def paths(m, n):
     >>> paths(1, 157)
     1
     """
-    "*** YOUR CODE HERE ***"
+    if m == 1 or n == 1:
+        return 1
+    return paths(m - 1, n) + paths(m, n - 1)
 
 
 def pascal(row, column):
@@ -50,7 +54,13 @@ def pascal(row, column):
     >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
     6
     """
-    "*** YOUR CODE HERE ***"
+    if column > row:
+        return 0
+    
+    if column == 0 or row <= 1:
+        return 1
+    
+    return pascal(row - 1, column - 1) + pascal(row - 1, column)
 
 
 def double_eights(n):
@@ -74,4 +84,8 @@ def double_eights(n):
     >>> check(HW_SOURCE_FILE, 'double_eights', ['While', 'For'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if n < 88:
+        return False
+    if n % 100 == 88:
+        return True
+    return double_eights(n // 10) 
